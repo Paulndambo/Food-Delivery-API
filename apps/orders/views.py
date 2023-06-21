f57f15e8d69c420c1da874361bf7ca37bbbb1b89
Paul Ndambo <52633424+Paulndambo@users.noreply.github.com>
2023-06-22 02:33:21 +0300

    orders endpoint permissions check added

--- apps/orders/views.py
+++ apps/orders/views.py
@@ -8,11 +8,24 @@ from apps.orders.models import Order, OrderItem
 from apps.orders.serializers import OrderSerializer
 # Create your views here.
 class OrderViewSet(ModelViewSet):
+    """
+    : Order model maps to the Order table on the db,
+    : Orders endpoint should be protected since the response data is specific to;-
+    :   -> Customer
+    :   -> Restaurant
+    """
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
+        """
+        : From the current request, get the authenticated user
+        : Specific user role determines the orders data they will get upon a get request
+        : -> Customer, only gets data on order they have made
+        : -> Restaurant, only gets data on orders made on the specific restaurant
+        : -> Admin, This user can get all order data on orders
+        """
         user = self.request.user
 
         if user.role.lower() == "customer":
@@ -22,12 +35,3 @@ class OrderViewSet(ModelViewSet):
         elif user.role.lower() in ["admin", "customer_service"] or user.is_superuser:
             return self.queryset
 
-
-    #def create(self, request, *args, **kwargs):
-    #    data = request.data
-    #    serializer = self.serializer_class(data=data)
-    #    if serializer.is_valid(raise_exception=True):
-    #        order_items = serializer.validated_data["order_items"]
-    #        print(order_items)
-    #        return Response(serializer.data, status=status.HTTP_201_CREATED)
-    #    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
